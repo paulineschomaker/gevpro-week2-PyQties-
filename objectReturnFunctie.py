9dcc40013640a7f6aca6a877c0a3d0735db74e75
Pauline <paulineschomaker@hotmail.com>
2015-02-22 16:03:54 +0100

    Programma werkt nu. Heb flag color toegevoegd. Volgens mij return je functie nog geen lijst met landen

--- objectReturnFunctie.py
+++ objectReturnFunctie.py
@@ -1,26 +1,36 @@
 #!/usr/bin/env python
 
 import sys
+from flag_color import *
+from PyQt4 import QtCore, QtGui
 
 # Dit is een Country-class, doet echt weinig stoers.
 class Country():
-    def __init__(self, country):
+    def __init__(self, country, countryFile):
         self.country = country
+        self.countryFile = countryFile
     
     def __str__(self):
         return self.country
-
+        
 # Dit is de functie die lijsten van Country's maakt (countryList)
-def putYoCountriesInAList(countryFile):
-    infile = open(countryFile, "r")
-    countryList = []
-    
-    for line in infile:
-        newCountry = Country(line) #haha, Country(line)dancing.
-        countryList.append(newCountry)
-    
-    return countryList
-
-if __name__ == '__main__':
-    putYoCountriesInAList(countries_list.txt)
+    def putYoCountriesInAList(countryFile):
+        infile = open(countryFile, "r")
+        countryList = []
+	    
+        for line in infile:
+            newCountry = Country(line) #haha, Country(line)dancing.
+            countryList.append(newCountry)
+            return countryList
+            
+# Dit is de functie die de kleur bepaalt    
+    def flagColor():
+         color = flag_color()
+      
+def main():
+    country = Country("Landje", "countries_list.txt") 
     print(country)
+         
+if __name__ == '__main__':
+	main()
+    
