9c0d2d37586bc039fb787b89c082fb3b7a990efe
Pauline <paulineschomaker@hotmail.com>
2015-02-22 19:05:56 +0100

    Echt geweldig dit programma

--- flag_color.py
+++ flag_color.py
@@ -5,11 +5,11 @@ from PyQt4 import QtCore, QtGui
 class FlagColor(QtGui.QColor):
     def __init__(self):
         super().__init__()
-        self.setRandomColor()
     
-    def setRandomColor(self):
-        self.setBlue(randrange(0,255))
-        self.setGreen(randrange(0,255))
-        self.setRed(randrange(0,255))
+def setRandomColor(self):
+    self.setBlue(randrange(0,255))
+    self.setGreen(randrange(0,255))
+    self.setRed(randrange(0,255))
+       
 
 
