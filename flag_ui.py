a5453008bba4f01a88e6772ce93b079b7a4ed5c3
Pauline <paulineschomaker@hotmail.com>
2015-02-23 01:15:20 +0100

    comments toegevoegd

--- flag_ui.py
+++ flag_ui.py
@@ -10,6 +10,7 @@ import sys
 
 class CountryWindow(QDialog):
     def __init__(self):
+        """Maakt een Interface aan met Combobox en QFrame"""
         QWidget.__init__(self)
         self.countriesFile = "countries_list.txt"
         self.countriesList = countriesFileToList(self.countriesFile)
@@ -24,39 +25,38 @@ class CountryWindow(QDialog):
             self.countryPicker.addItem(thisCountry.country)
         self.countryPicker.currentIndexChanged.connect(self.updateUI)
         
-        #QFrame
+        #QFrame bovenste balk vlag
         self.countryFrame = QtGui.QFrame(self)
         self.countryFrame.setGeometry(250,50,120,30)
         self.countryFrame.move(90,70)
         
+        #QFrame middelste balk vlag
         self.countryFrame2 = QtGui.QFrame(self)
         self.countryFrame2.setGeometry(250,50,120,30)
         self.countryFrame2.move(90,100)
         
+        #QFrame onderste balk vlag
         self.countryFrame3 = QtGui.QFrame(self)
         self.countryFrame3.setGeometry(250,50,120,30)
         self.countryFrame3.move(90,130)
         
-        
-        #Color
-        self.Color = QtGui.QColor(200,5,20)
-        
-        # Grid #
+        #Window
         self.setGeometry(500, 200, 300, 300)
         self.setWindowTitle('Country')
         self.show()
         
         #Update
     def updateUI(self):
+        """Deze functie zoekt de kleuren die bij het land horen en stelt de kleur in"""
         newCountry = self.countryPicker.currentText()
-        for findCountry in self.countriesList:
+        for findCountry in self.countriesList:     #bovenste balk
             if (findCountry.country == newCountry):
                 self.countryFrame.setStyleSheet("QFrame { background-color: %s }" % findCountry.color.name())
-        newCountry2 = self.countryPicker.currentText()
+        newCountry2 = self.countryPicker.currentText() #middelste balk
         for findCountry in self.countriesList2:
             if (findCountry.country == newCountry2):        
                 self.countryFrame2.setStyleSheet("QFrame { background-color: %s }" % findCountry.color.name())
-        newCountry3 = self.countryPicker.currentText()
+        newCountry3 = self.countryPicker.currentText() #onderste balk
         for findCountry in self.countriesList3:
             if (findCountry.country == newCountry3):        
                 self.countryFrame3.setStyleSheet("QFrame { background-color: %s }" % findCountry.color.name())
