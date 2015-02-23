ab1037606070f2819cb4c1409e118d7a30ad8eb6
Pauline <paulineschomaker@hotmail.com>
2015-02-23 01:10:28 +0100

    de vlag heeft nu drie kleuren (swag)

--- flag_ui.py
+++ flag_ui.py
@@ -13,25 +13,37 @@ class CountryWindow(QDialog):
         QWidget.__init__(self)
         self.countriesFile = "countries_list.txt"
         self.countriesList = countriesFileToList(self.countriesFile)
+        self.countriesList2 = countriesFileToList(self.countriesFile)
+        self.countriesList3 = countriesFileToList(self.countriesFile)
  
         # UI #
         #Combobobox
         self.countryPicker = QtGui.QComboBox(self)
+        self.countryPicker.move(40,20)
         for thisCountry in self.countriesList:
             self.countryPicker.addItem(thisCountry.country)
         self.countryPicker.currentIndexChanged.connect(self.updateUI)
         
         #QFrame
         self.countryFrame = QtGui.QFrame(self)
-        self.countryFrame.setGeometry(250,50,50,25)
-        self.countryFrame.move(10,50)
+        self.countryFrame.setGeometry(250,50,120,30)
+        self.countryFrame.move(90,70)
+        
+        self.countryFrame2 = QtGui.QFrame(self)
+        self.countryFrame2.setGeometry(250,50,120,30)
+        self.countryFrame2.move(90,100)
+        
+        self.countryFrame3 = QtGui.QFrame(self)
+        self.countryFrame3.setGeometry(250,50,120,30)
+        self.countryFrame3.move(90,130)
+        
         
         #Color
         self.Color = QtGui.QColor(200,5,20)
         
         # Grid #
         self.setGeometry(500, 200, 300, 300)
-        self.setWindowTitle('Raampje')
+        self.setWindowTitle('Country')
         self.show()
         
         #Update
@@ -40,7 +52,15 @@ class CountryWindow(QDialog):
         for findCountry in self.countriesList:
             if (findCountry.country == newCountry):
                 self.countryFrame.setStyleSheet("QFrame { background-color: %s }" % findCountry.color.name())
-        
+        newCountry2 = self.countryPicker.currentText()
+        for findCountry in self.countriesList2:
+            if (findCountry.country == newCountry2):        
+                self.countryFrame2.setStyleSheet("QFrame { background-color: %s }" % findCountry.color.name())
+        newCountry3 = self.countryPicker.currentText()
+        for findCountry in self.countriesList3:
+            if (findCountry.country == newCountry3):        
+                self.countryFrame3.setStyleSheet("QFrame { background-color: %s }" % findCountry.color.name())
+				
         
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
